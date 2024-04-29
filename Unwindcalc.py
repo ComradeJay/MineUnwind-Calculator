83eb1bbe81edeadcef7369484d920bbbc1d70564
ComradeJay <82870218+ComradeJay@users.noreply.github.com>
2024-04-29 15:48:38 +0100

    Update Unwindcalc.py

--- Unwindcalc.py
+++ Unwindcalc.py
@@ -33,6 +33,13 @@ if dim=="City": # 𝟭𝘀𝘁 𝗥𝗲𝗮𝗹𝗺
                st.header(f"You need {b:,d} Stacks of T3 Diamond and Emerald")
           else:
                st.header(f"You need {b:,d} stacks and {c} blocks of T3 Diamond and Emerald ")
+     if block=='City Mix (Mix only)':
+          a = int(val / 64)
+          b = val % 64
+          if ((b % 64)==0):
+               st.header(f"You need {a:,d} Stacks of T3 Stone and Iron")
+          else:
+               st.header(f"You need {a:,d} stacks and {b} blocks of T3 Stone and Iron")
      else:
           Diamond = val*36
           Emerald = val*24
