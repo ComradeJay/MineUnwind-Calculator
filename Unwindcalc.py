7286be8e1bd369bbab90ff552b0e1cfebe2aa22b
ComradeJay <82870218+ComradeJay@users.noreply.github.com>
2024-04-29 15:41:42 +0100

    Update Unwindcalc.py

--- Unwindcalc.py
+++ Unwindcalc.py
@@ -43,13 +43,13 @@ if dim=="City": # 𝟭𝘀𝘁 𝗥𝗲𝗮𝗹𝗺
           else:
                st.header(f"You need {int(Iron / 64):,d} Stacks and {Iron % 64} T3 Iron")
           if ((Diamond % 64)==0):
-               st.header(f"You need {int(Diamond / 64):,d} Stacks of T3 Frozen Diamond")
+               st.header(f"You need {int(Diamond / 64):,d} Stacks of T3 Diamond")
           else:
-               st.header(f"You need {int(Diamond/64):,d} Stacks and {Diamond%64} T3 Frozen Diamond")
+               st.header(f"You need {int(Diamond/64):,d} Stacks and {Diamond%64} T3 Diamond")
           if ((Emerald % 64)==0):
-               st.header(f"You need {int(Emerald / 64):,d} Stacks of T3 Frozen Emerald")
+               st.header(f"You need {int(Emerald / 64):,d} Stacks of T3 Emerald")
           else:
-               st.header(f"You need {int(Emerald / 64):,d} Stacks and {Emerald % 64} T3 Frozen Emerald")
+               st.header(f"You need {int(Emerald / 64):,d} Stacks and {Emerald % 64} T3 Emerald")
           st.header(f"You need {int(City / 64):,d} Stacks of Iron and Diamond Mix and {int(City / 64):,d} Stacks of Diamond and Emerald Mix")
 
 
