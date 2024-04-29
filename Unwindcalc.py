a509de719570143a573d438cbaf45202dd0fcd71
ComradeJay <82870218+ComradeJay@users.noreply.github.com>
2024-04-29 17:56:39 +0100

    Update Unwindcalc.py

--- Unwindcalc.py
+++ Unwindcalc.py
@@ -34,25 +34,22 @@ if dim=="City": # 𝟭𝘀𝘁 𝗥𝗲𝗮𝗹𝗺
           else:
                st.header(f"You need {b:,d} stacks and {c} blocks of T3 Diamond and Emerald ")
      elif block=='City Mix (Mix only)':
-          Diamond = val*36
-          Emerald = val*24
-          Iron = val*12
-          dia=(12)
-          em=(8)
-          fe=(4)
+          diamond = val*12
+          emerald = val*8
+          iron = val*4
           City=(24*3)
           if ((Iron % 64)==0):
-               st.header(f"You need {int(Iron / 64):,d} Stacks of T3 Iron {City}")
+               st.header(f"You need {int(Iron % 64):,d} Stacks of T3 Iron {City}")
           else:
-               st.header(f"You need {int(Iron / 64):,d} Stacks and {Iron % 64} T3 Iron")
+               st.header(f"You need {int(Iron % 64):,d} Stacks and {Iron % 64} T3 Iron")
           if ((Diamond % 64)==0):
-               st.header(f"You need {int(Diamond / 64):,d} Stacks of T3 Diamond")
+               st.header(f"You need {int(Diamond % 64):,d} Stacks of T3 Diamond")
           else:
-               st.header(f"You need {int(Diamond/64):,d} Stacks and {Diamond%64} T3 Diamond")
+               st.header(f"You need {int(Diamond%64):,d} Stacks and {Diamond%64} T3 Diamond")
           if ((Emerald % 64)==0):
-               st.header(f"You need {int(Emerald / 64):,d} Stacks of T3 Emerald")
+               st.header(f"You need {int(Emerald % 64):,d} Stacks of T3 Emerald")
           else:
-               st.header(f"You need {int(Emerald / 64):,d} Stacks and {Emerald % 64} T3 Emerald")
+               st.header(f"You need {int(Emerald % 64):,d} Stacks and {Emerald % 64} T3 Emerald")
      
      
      
