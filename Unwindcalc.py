9b30997166dd1fcf3bed731386650e5757b1d7b9
ComradeJay <82870218+ComradeJay@users.noreply.github.com>
2024-04-29 15:18:24 +0100

    Update Unwindcalc.py

--- Unwindcalc.py
+++ Unwindcalc.py
@@ -37,7 +37,7 @@ if dim=="City": # 𝟭𝘀𝘁 𝗥𝗲𝗮𝗹𝗺
           Diamond = val*36
           Emerald = val*24
           Iron = val*12
-          City = int(val * 64)
+          City = int(val * 3)
           if ((Iron % 64)==0):
                st.header(f"You need {int(Iron / 64):,d} Stacks of T3 Iron")
           else:
