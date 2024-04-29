c721ea535a009b61ea1190d665129be3d75db63d
ComradeJay <82870218+ComradeJay@users.noreply.github.com>
2024-04-29 15:01:05 +0100

    Update Unwindcalc.py

--- Unwindcalc.py
+++ Unwindcalc.py
@@ -54,7 +54,7 @@ if dim=="City": # 𝟭𝘀𝘁 𝗥𝗲𝗮𝗹𝗺
           if ((ID % 64)==0):
                st.header(f"You need {int(ID / 64):,d} Stacks of Iron - Diamond Mix")
           if ((DE % 64)==0):
-               st.header(f"You need {int(ID / 64):,d} Stacks of Diamond - Emerald Mix")
+               st.header(f"You need {int(DE / 64):,d} Stacks of Diamond - Emerald Mix")
           
          #st.header(f"You need {ID % 64} Stacks of Iron and Diamond Mix and {DE % 64} Stacks of Diamond and Emerald Mix")
 
