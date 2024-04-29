aec02d8fe2bde897f9a8af67aae6aa2f1de5e416
ComradeJay <82870218+ComradeJay@users.noreply.github.com>
2024-04-29 18:13:53 +0100

    Update Unwindcalc.py

--- Unwindcalc.py
+++ Unwindcalc.py
@@ -38,7 +38,7 @@ if dim=="City": # 𝟭𝘀𝘁 𝗥𝗲𝗮𝗹𝗺
           DE = val*24
           City = (ID+DE/64)
           if ((ID % 64)==0):
-               st.header(f"You need {int(ID / 64):,d} Stacks of Iron - Diamond Mix {City}")
+               st.header(f"You need {int(ID / 64):,d} Stacks of Iron - Diamond Mix")
           else:
                st.header(f"You need {int(ID / 64):,d} Stacks if Iron - Diamond Mix and {ID % 64} T3 Iron")
           if ((DE % 64)==0):
